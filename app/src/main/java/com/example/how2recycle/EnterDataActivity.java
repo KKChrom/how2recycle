6b4ba167c7fce4a88d2d6145692ed78891408181
sebastiantroncoso <sctroncoso10@gmail.com>
2020-01-05 11:23:20 -0800

    Slight changes in development

--- app/src/main/java/com/example/how2recycle/EnterDataActivity.java
+++ app/src/main/java/com/example/how2recycle/EnterDataActivity.java
@@ -1,13 +1,8 @@
 package com.example.how2recycle;
 
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
-
-public class EnterDataActivity extends AppCompatActivity {
-
 import android.content.Intent;
-import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
 
