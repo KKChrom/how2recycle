91e65cf1fb61fc0da2325b7a294d84e0c455090d
sam <sam@sam.com>
2020-01-05 11:28:08 -0800

    videoattemp and merge

--- app/src/main/java/com/example/how2recycle/EnterDataActivity.java
+++ app/src/main/java/com/example/how2recycle/EnterDataActivity.java
@@ -2,9 +2,6 @@ package com.example.how2recycle;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
-
-public class EnterDataActivity extends AppCompatActivity {
 
 import android.content.Intent;
 import android.os.Bundle;
