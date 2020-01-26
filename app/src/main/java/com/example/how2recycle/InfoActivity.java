6c96b239091f9433435cff300be2bf26deed234d
sam <sam@sam.com>
2020-01-26 11:29:23 -0800

    got the number and strings set up

--- app/src/main/java/com/example/how2recycle/InfoActivity.java
+++ app/src/main/java/com/example/how2recycle/InfoActivity.java
@@ -4,14 +4,29 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+
 public class InfoActivity extends AppCompatActivity {
+    public static final String EXTRA_RECYCLE_NUMBER =
+            "com.example.how2recycle.extra.RECYCLE_NUMBER";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_info);
-        String val = getIntent().getStringExtra("EXTRA_RECYCLE_NUMBER");
+        ArrayList<String> s = new ArrayList<String>();
+
+        String val = getIntent().getStringExtra(EXTRA_RECYCLE_NUMBER);
+
         System.out.println(val+" is the value passed ");
+        switch (val)
+        {
+            case 1:
+
+
+
+        }
     }
 
 }
