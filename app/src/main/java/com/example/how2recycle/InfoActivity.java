bbae768ae941f71dd73d6b298ebcfeb3c9e8ed59
sam <sam@sam.com>
2020-02-09 10:53:22 -0800

    completed info activity

--- app/src/main/java/com/example/how2recycle/InfoActivity.java
+++ app/src/main/java/com/example/how2recycle/InfoActivity.java
@@ -2,10 +2,11 @@ package com.example.how2recycle;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.nfc.Tag;
 import android.os.Bundle;
-import android.widget.Switch;
-
-import java.util.ArrayList;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class InfoActivity extends AppCompatActivity {
     public static final String EXTRA_RECYCLE_NUMBER =
@@ -15,41 +16,49 @@ public class InfoActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_info);
-        String desc;
-        String path;
+        ImageView imageView = findViewById(R.id.imageView);
+        TextView textView = findViewById(R.id.desc);
+
         String val = getIntent().getStringExtra(EXTRA_RECYCLE_NUMBER);
         switch (val) {
-            case "1":
-                path = "raw/rone.png";
-
-
+            case ("1"):
+                imageView.setImageResource(R.drawable.rone);
+                textView.setText("Polyethylene terephthalate(PETE/PET) is the material that is used in the packaging of foods and beverages");
                 break;
 
             case "2":
-                path = "raw/rtwo.png";
+                textView.setText("High Density polyethylene(HDPE) is used in bottles, cups. milk jugs, piping, plastic lumber and many more day to day things");
+                imageView.setImageResource(R.drawable.rtwo);
                 break;
 
             case "3":
-                path = "raw/rthree.png";
+                imageView.setImageResource(R.drawable.rthree);
+                textView.setText("Polyvinyl chloride(PVC) is used in the industry in things like pipes, sides, and flooring");
                 break;
 
             case "4":
-                path = "raw/rfour.png";
+                imageView.setImageResource(R.drawable.rfour);
+                textView.setText("Low-density polyethylene(LDPE) is used in a lot of commonplace objects such as plastic bags, six-pack rings, and tubing");
                 break;
 
             case "5":
-                path = "raw/rfive.png";
-
+                imageView.setImageResource(R.drawable.rfive);
+                textView.setText("Polypropylene(PP) is used in the industry in things like auto parts, industrial fibres, and food containers");
                 break;
 
             case "6":
-                path = "raw/rsix.png";
+                imageView.setImageResource(R.drawable.rsix);
+                textView.setText("Polystyrene(PS) is used in lots of daytoday things like plastic utensils, styrofoam, and cafeteria trays");
                 break;
 
             case "7":
-                path = "raw/rseven.png";
+                imageView.setImageResource(R.drawable.rseven);
+                textView.setText("This is the name of other plastics(Acrylic, Nylon, Polycarbonate, and Polylactic Acid) which are used in a variety of different objects");
                 break;
+            default:
 
+                Toast.makeText(getApplicationContext(),"Need values between 1 and 7", Toast.LENGTH_SHORT).show();
+                break;
 
         }
 
