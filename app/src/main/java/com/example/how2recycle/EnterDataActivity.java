6d7a76357122c843d54c0e4e16d44cc537cecbbd
DanielR1 <activatingredstone@gmail.com>
2020-02-23 11:08:14 -0800

    Working on the text

--- app/src/main/java/com/example/how2recycle/EnterDataActivity.java
+++ app/src/main/java/com/example/how2recycle/EnterDataActivity.java
@@ -14,6 +14,8 @@ public class  EnterDataActivity extends AppCompatActivity {
             "com.example.how2recycle.extra.RECYCLE_NUMBER";
     private EditText mNumberEditText;
 
+    //Some change
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
